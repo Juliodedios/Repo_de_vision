6e42f37f21adbe3e3d4c2f145a5806494dbb3f82
Juliodedios <jcesarupp93@gmail.com>
2024-02-28 14:35:57 -0600

    project: Repo_de_vision

--- edges_follow.py
+++ edges_follow.py
@@ -40,7 +40,7 @@ upper=int(max(255,1.3*med_valor))
 bordes_2=cv2.Canny(img_dist,threshold1=lower,threshold2=upper)
 bordes_3=cv2.Canny(imgrgb,threshold1=lower,threshold2=upper)
 countours_b,jerarqui_b=cv2.findContours(bordes_1,cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
-#print(countours_b)
+
 cv2.drawContours(imgrgb,countours_b,-1,(0,255,0),3)
 cv2.imshow("Imagen",imgrgb)
 #count_txt = np.savetxt('count.txt', countours_b, delimiter =', '
